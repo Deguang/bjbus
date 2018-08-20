455178813de492ba7894c0cc89b8b0e9fd2aac2e
Deguang <better_li@live.com>
2018-08-20 23:53:42 +0800

    test for navigator

--- lib/main.dart
+++ lib/main.dart
@@ -3,80 +3,39 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:bjbus/detail.dart';
 
-Future<Post> fetchPost() async {
-  final _url = 'http://www.bjbus.com/home/ajax_rtbus_data.php?act=busTime&selBLine=621&selBDir=4746130842978198564&selBStop=11';
-  final response =
-      await http.get(_url);
-
-  if (response.statusCode == 200) {
-    // If the call to the server was successful, parse the JSON
-    return Post.fromJson(json.decode(response.body));
-  } else {
-    // If that call was not successful, throw an error.
-    throw Exception('Failed to load post');
-  }
+void main() {
+  runApp(new MaterialApp(
+    title: 'Navigation basics',
+    home: new Home()
+  ));
 }
 
-class Post {
-  final int userId;
-  final int id;
-  final String title;
-  final String body;
-
-  Post({this.userId, this.id, this.title, this.body});
-
-  factory Post.fromJson(Map<String, dynamic> json) {
-    return Post(
-      userId: json['userId'],
-      id: json['id'],
-      title: json['title'],
-      body: json['body'],
-    );
-  }
-}
-
-void main() => runApp(new MyApp());
-
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: '北京实时公交',
-      theme: new ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or press Run > Flutter Hot Reload in IntelliJ). Notice that the
-        // counter didn't reset back to zero; the application is not restarted.
-        primarySwatch: Colors.blue,
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('北京实时公交'),
       ),
-      home: new BusStateList(),
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text('Lanch new screen'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              new MaterialPageRoute(builder: (context) => new Detail())
+            );
+            // Navigate to second screen when tapped!
+          }
+        ),
+      ),
     );
   }
 }
 
-class BusStateList extends StatefulWidget {
-  @override
-  BusState createState() => new BusState();
+Future<http.Response> fetchObject() {
+  return http.get('http://www.bjbus.com/home/ajax_rtbus_data.php?act=busTime&selBLine=966&selBDir=5437790928461106576&selBStop=18');
 }
 
-class BusState extends State<BusStateList> {
-  final _busNo = "621";
-
-  @override
-  Widget build(BuildContext context) {
-
-    return new Row(
-      children: <Widget>[
-        new Text(
-          _busNo + '路'
-        )
-      ],
-    );
-  }
-}
